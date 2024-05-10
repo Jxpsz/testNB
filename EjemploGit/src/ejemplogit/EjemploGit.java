33529e81047cc4437aa64a0a988fccadd32b3f58
Jxpsz <jhonh.perezr@konradlorenz.edu.co>
2024-05-10 13:02:22 -0500

    Update EjemploGit.java
    no se hace

--- EjemploGit/src/ejemplogit/EjemploGit.java
+++ EjemploGit/src/ejemplogit/EjemploGit.java
@@ -7,7 +7,7 @@ public class EjemploGit {
 
     public static void main(String[] args) {
         System.out.println("Hola GitHub ☺");
-        System.out.println("");
+        System.out.println("esto no se hace");
         System.out.println("");
         int n =5;
 

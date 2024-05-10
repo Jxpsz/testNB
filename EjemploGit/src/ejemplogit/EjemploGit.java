858638cb4c035e4d1d07a77e65c3c0f0bc9c62ef
Usuario <Usuario@EDI102EST14>
2024-05-10 12:45:52 -0500

    commit 1: Muestra 3 mensajes

---
+++ EjemploGit/src/ejemplogit/EjemploGit.java
@@ -0,0 +1,17 @@
+
+package ejemplogit;
+
+
+public class EjemploGit {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola GitHub ☺");
+        System.out.println("");
+        System.out.println("");
+        int n =5;
+
+    }
+    
+    
+}
